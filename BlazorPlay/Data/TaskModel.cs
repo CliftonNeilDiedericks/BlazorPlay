953dec4a7816b0877624e896d5cda8e31216f897
Clifton Neil Diedericks <007Neilo@gmail.com>
2021-06-20 23:46:39 +0200

    TaskList with progressbar for complete tasks

---
+++ BlazorPlay/Data/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPlay.Data
+{
+    public class TaskModel
+    {
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
